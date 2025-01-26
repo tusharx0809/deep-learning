b31ddeee483e6ed60566c604da97a5376a1c8911
tusharx0809 <tusharbhatia0809@outlook.com>
2025-01-26 11:02:50 +0530

    correct dataset downloaded

--- projects/DogBreedsClassification/download.py
+++ projects/DogBreedsClassification/download.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("abhinavkrjha/dog-breed-classification")
+path = kagglehub.dataset_download("eward96/dog-breed-images")
 
 print("Path to dataset files:", path)
